fa56f65d7e5928dab0431e9b23b6446c0709a790
Victor <victornjoroge2020@gmail.com>
2023-07-08 17:05:32 +0300

    Activation of Db.json

--- beach.js
+++ beach.js
@@ -14,16 +14,8 @@ const Individual14=(item)=>{
     let container=document.createElement("div")
     container.innerHTML=`
     <div class="box">
-    <img src="${item.images[0]}">
-    <p class="title">${item.name}</p>
-    <span>${item.city}</span>
-    <div class="details">
-    <p>Bedrooms ${item.bedrooms}</p>
-    <p>Bathrooms ${item.bathrooms}</p>
-    <p> Beds ${item.beds}</p>
-    <br>
-    </div>
-    <p> Address ${item.address}</p>
+    <img src="${item.image}">
+  
     <button class="book">book</button>
    </div>
     `
@@ -41,24 +33,10 @@ slide()
 
 
 
-async function beach(){
-    const url = 'https://airbnb13.p.rapidapi.com/search-location?location=Beach&checkin=2023-09-16&checkout=2023-09-17&adults=1&children=0&infants=0&pets=0&page=1&currency=USD';
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '9ecbe81670msh499afdf84707a0ep18e99ajsn912bd10d6138',
-            'X-RapidAPI-Host': 'airbnb13.p.rapidapi.com'
-        }
-    }
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        result.results.forEach((item) => {
-           Individual14(item);
-         });
-
-    } catch (error) {
-        console.error(error);
-    }
-  
+ function beach(){
+  fetch("http://localhost:3000/Beach")
+  .then((res)=>{
+    res.json()
+    .then(Beach=>Beach.forEach(item=>Individual14(item)))
+  })
 }
