338ae2238264b4c8197425824b8ad144139e56d9
Victor <victornjoroge2020@gmail.com>
2023-07-10 19:08:18 +0300

    afternoon commit

--- db.js
+++ db.js
@@ -15,19 +15,67 @@ const Individual3=(item)=>{
     container.innerHTML=`
     <div class="box">
     <img src="${item.image}">
+    <p>${item.Adress}</p>
+    <p>${item.name}</p>
+    <div>
+    <p>${item.Bedroom}</p>
+    <p>${item.Beds}</p>
+    <p>${item.Bathrooms}</p>
+    <p class="Status">Status <span>${item.Status}</span></p>
+    </div>
     <button class="book">book</button>
+    <button class="delete"><i class="fas fa-trash"></i></button>
    </div>
     `
-    document.querySelector(".box-container").append(container);
     document.querySelector(".box-container").append(container);
     function slide(){
         container.querySelector(".book").addEventListener("click", ()=>{
       let reservation=document.querySelector(".login")
       reservation.style.display="block"
-   
+
+
+
+
+    const booking=()=>{
+      let reserved=document.querySelector(".reserveBtn")
+      reserved.addEventListener("click", (e)=>{
+        e.preventDefault()
+      item.Status="Reserved"
+      container.querySelector(".Status").textContent=item.Status
+        fetch(`http://localhost:3000/swimming/${item.id}`,{
+          method:"PATCH",
+          headers:{
+            "Content-Type":"application/json"
+          },
+          body:JSON.stringify({Status:item.Status})
+        })
+
+
+
+
+      })
+    }
+
+    booking()
   })
   }
 slide()
+const Delete=()=>{
+  container.querySelector(".delete").addEventListener("click", ()=>{
+    container.remove()
+    fetch(`http://localhost:3000/swimming/${item.id}`,{
+          method:"DELETE",
+          headers:{
+            "Content-Type":"application/json"
+          }
+        })
+        .then(res =>res.json())
+        .then(Rooms=>console.log(Rooms))
+  })
+}
+Delete()
+
+
 }
 
 
